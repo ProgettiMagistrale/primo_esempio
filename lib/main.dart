349309b5ed1d134b984fe50e21635bc6f3a3a43f
ProgettiMagistrale <ernesto.petroni@liber.it>
2021-11-01 12:44:14 +0100

    ernestolavati

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:primo_esempio/map_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Google Maps Web',
       home: MapScreen(),
     );
