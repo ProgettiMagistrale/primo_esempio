43249921c53803f870d6b8baa5024c77a1fd11e7
ProgettiMagistrale <ernesto.petroni@liber.it>
2021-11-01 10:26:53 +0100

    a

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times bella zio:',
+              'You have pushed the button this many times bella:',
             ),
             Text(
               '$_counter',
