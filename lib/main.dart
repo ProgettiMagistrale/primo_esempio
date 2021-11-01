fc545761ddbdea197d8a59446be472cf28e1ddd1
ProgettiMagistrale <ernesto.petroni@libero.it>
2021-11-01 11:08:13 +0100

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Ernesto '),
+      home: const MyHomePage(title: 'Hello World'),
     );
   }
 }
