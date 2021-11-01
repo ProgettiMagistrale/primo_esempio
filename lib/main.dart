f5f0836b8021372e7d100c90608b996b22739eeb
ProgettiMagistrale <93515862+ProgettiMagistrale@users.noreply.github.com>
2021-11-01 10:49:40 +0100

    'Modifica'

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page Ciao'),
     );
   }
 }
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times bella:',
             ),
             Text(
               '$_counter',
